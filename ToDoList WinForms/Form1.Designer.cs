2c6aedc1ff0e5d4808fb8de661c36ad0654ed693
Sebastia n <KamashiVentures@gmail.com>
2025-03-11 20:18:04 +0100

    Add project files.

---
+++ ToDoList WinForms/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace ToDoList_WinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(-2, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 61);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add new task!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(180, 1);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(622, 454);
+            this.checkedListBox1.TabIndex = 2;
+            this.checkedListBox1.Click += new System.EventHandler(this.checkedListBox1_Click);
+            this.checkedListBox1.MouseHover += new System.EventHandler(this.checkedListBox1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(-2, 217);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(182, 55);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Remove at";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(-2, 59);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(182, 120);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(-2, 278);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(182, 55);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Save ToDoList";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(-2, 339);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(182, 55);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Load ToDoList";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(-2, 400);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(182, 55);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Exit";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseHover += new System.EventHandler(this.checkedListBox1_Click);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ ToDoList WinForms/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ToDoList_WinForms
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<int, string> originalItems; // Słownik do przechowywania oryginalnych treści
+
+        public Form1()
+        {
+            InitializeComponent();
+            originalItems = new Dictionary<int, string>(); // Inicjalizacja słownika
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            
+            string newItem = richTextBox1.Text;
+            checkedListBox1.Items.Add(newItem);
+            originalItems[checkedListBox1.Items.Count - 1] = newItem; // Zapisanie oryginalnego tekstu
+            Refresh();
+        }
+
+        void Refresh()
+        {
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                if (checkedListBox1.GetItemChecked(i))
+                {
+                    if (!checkedListBox1.Items[i].ToString().Contains("✔️")) // Zapobiega wielokrotnemu dodawaniu
+                    {
+                        checkedListBox1.Items[i] = originalItems[i] + " ✔️ " + DateTime.Now;
+                    }
+                }
+                else
+                {
+                    checkedListBox1.Items[i] = originalItems[i]; // Przywracanie oryginalnego tekstu
+                }
+            }
+        }
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void checkedListBox1_Click(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+    }
+}
